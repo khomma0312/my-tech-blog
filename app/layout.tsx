993aea3eb921330479ef9050d8c85f84bcb17094
khomma0312 <kijikuroneko@gmail.com>
2024-10-23 06:45:31 +0900

    feat: 全体のレイアウトとカラーを追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/layouts/header";
+import Footer from "@/components/layouts/footer";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -10,7 +12,7 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: {
     template: "%s | My Tech Blog",
-    default: "ホーム | My Tech Blog",
+    default: "Home | My Tech Blog",
   },
   description: "Web系の技術情報を投稿するブログです。",
 };
@@ -22,8 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className={`${inter.className} antialiased bg-background`}>
-        {children}
+      <body
+        className={`${inter.className} antialiased bg-background min-h-screen grid grid-rows-[auto_1fr_auto]`}
+      >
+        <Header />
+        <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
