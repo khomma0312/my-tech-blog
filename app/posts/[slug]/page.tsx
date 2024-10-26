e9a73c5556c47dea17200028a597d5d747073cb7
khomma0312 <kijikuroneko@gmail.com>
2024-10-26 22:56:02 +0900

    feat: 記事詳細ページのスタイリングとコードハイライト部分のロジック、スタイルを修正

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import { getPostBySlug } from "@/features/posts/api";
 import { markdownToHtml } from "@/utils/markdown-to-html";
+import "highlight.js/styles/github-dark.css";
+import styles from "@/styles/post.module.css";
 
 type Props = {
   params: {
@@ -15,7 +17,11 @@ const Post = async ({ params }: Props) => {
   // markdownからHTMLに変換
   const content = await markdownToHtml(post.content || "");
 
-  return <article>{content}</article>;
+  return (
+    <div className="mx-auto max-w-prose">
+      <article className={styles.markdown}>{content}</article>
+    </div>
+  );
 };
 
 export default Post;
--- lib/marked.ts
+++ lib/marked.ts
@@ -1,19 +1,48 @@
 import { Marked } from "marked";
-import { markedHighlight } from "marked-highlight";
 import hljs from "highlight.js";
 
-const marked = new Marked(
-  markedHighlight({
-    emptyLangClass: "hljs",
-    langPrefix: "hljs language-",
-    highlight(code, lang) {
-      const language = hljs.getLanguage(lang) ? lang : "plaintext";
-      return hljs.highlight(code, { language }).value;
-    },
-  }),
-  {
-    gfm: true,
+const escapeTest = /[&<>"']/;
+const escapeReplace = new RegExp(escapeTest.source, "g");
+const escapeReplacements: { [index: string]: string } = {
+  "&": "&amp;",
+  "<": "&lt;",
+  ">": "&gt;",
+  '"': "&quot;",
+  "'": "&#39;",
+};
+const getEscapeReplacement = (ch: string) => escapeReplacements[ch];
+
+const escape = (html: string) => {
+  if (escapeTest.test(html)) {
+    return html.replace(escapeReplace, getEscapeReplacement);
   }
-);
+  return html;
+};
+
+const marked = new Marked({
+  gfm: true,
+  renderer: {
+    code({ text, lang }) {
+      const langString = (lang || "").match(/^\S*/)?.[0];
+      const fileInfo = langString ? langString.split(":") : [];
+      const fileName = fileInfo[1] ? fileInfo[1] : "";
+      const language = hljs.getLanguage(fileInfo[0] || "")
+        ? fileInfo[0]
+        : "plaintext";
+
+      const code = text.replace(/\n$/, "") + "\n";
+
+      return (
+        `<pre><span class="codeblock-file-info">${
+          fileName ? fileName : language
+        }</span><code class="hljs language-` +
+        escape(language) +
+        '">' +
+        hljs.highlight(code, { language }).value +
+        "</code></pre>\n"
+      );
+    },
+  },
+});
 
 export default marked;
