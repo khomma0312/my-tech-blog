b4e173600f2663e42b9bbead827367adda51c688
khomma0312 <kijikuroneko@gmail.com>
2024-10-28 07:04:58 +0900

    fix: 記事一覧ページへのリンク先を修正

--- components/posts/post-footer/index.tsx
+++ components/posts/post-footer/index.tsx
@@ -3,7 +3,10 @@ import Link from "next/link";
 const PostFooter = () => {
   return (
     <section className="py-10">
-      <Link href="/" className="text-primary hover:text-primary-500 text-sm">
+      <Link
+        href="/posts"
+        className="text-primary hover:text-primary-500 text-sm"
+      >
         &lt; 記事一覧に戻る
       </Link>
     </section>
