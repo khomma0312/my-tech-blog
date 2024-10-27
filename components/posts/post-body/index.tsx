21055d6d9c7eecedbee68f04af1394a707b12e5d
khomma0312 <kijikuroneko@gmail.com>
2024-10-28 06:06:37 +0900

    feat: 記事詳細ページで記事のヘッダー部分（タイトル、日時、タグ）の表示を追加

---
+++ components/posts/post-body/index.tsx
@@ -0,0 +1,13 @@
+import { cn } from "@/lib/utils";
+import "highlight.js/styles/github-dark.css";
+import styles from "@/styles/post.module.css";
+
+type Props = {
+  content: string | JSX.Element | JSX.Element[];
+};
+
+const PostBody = ({ content }: Props) => {
+  return <div className={cn(styles.markdown, "py-6")}>{content}</div>;
+};
+
+export default PostBody;
---
+++ components/posts/post-header/index.tsx
@@ -0,0 +1,30 @@
+import TagLink from "@/components/shared/tag-link";
+
+type Props = {
+  title: string;
+  date: string;
+  tags: string[];
+};
+
+const PostHeader = ({ title, date, tags }: Props) => {
+  return (
+    <div>
+      <h1 className="text-center text-3xl md:text-4xl font-bold py-8">
+        {title}
+      </h1>
+      <div className="flex justify-end pb-12 border-b border-slate-500">
+        <div className="flex flex-col">
+          <span className="text-slate-400 pb-2 text-sm">投稿日時: {date}</span>
+          <div className="flex gap-1 items-center justify-end">
+            <span className="text-slate-400 text-sm">タグ: </span>
+            {tags.map((tag: string) => (
+              <TagLink key={tag} tag={tag} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostHeader;
