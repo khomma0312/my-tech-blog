aef6fb2564ccd8dfa741ee63eba820109c28547c
khomma0312 <kijikuroneko@gmail.com>
2024-11-03 22:37:44 +0900

    feat: markdownファイル内のdateの降順で記事を表示するよう修正

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,27 @@
 import { getAllPostsSlug, getPostBySlug } from "@/features/posts/api";
 import PostCard from "@/components/posts/post-card";
 
+type Posts = {
+  slug: string;
+  title: string;
+  date: string;
+  tags?: string[];
+};
+
 const Home = async () => {
   const slugs = getAllPostsSlug();
-  const posts = slugs.map((slug) => {
-    const post = getPostBySlug(slug);
+  const posts: Posts[] = slugs.map((slug) => {
+    const { data } = getPostBySlug(slug);
     return {
       slug,
-      title: post.data.title,
-      date: post.data.date,
-      tags: post.data?.tags,
+      title: data.title,
+      date: data.date,
+      tags: data.tags,
     };
   });
+
+  // markdownファイル内のdateフィールドで降順にソート
+  posts.sort((a, b) => Date.parse(b.date) - Date.parse(a.date));
 
   return (
     <div className="max-w-screen-xl mx-auto px-6 pt-12 pb-32">
@@ -21,7 +31,7 @@ const Home = async () => {
         ))}
       </div>
 
-      {/* TODO: ページネーションをおく */}
+      {/* TODO: 記事数が増えてきたらページネーションをおく */}
     </div>
   );
 };
