108f0a8bf1189326b2fc7747c6526f2e6d9fb66a
khomma0312 <kijikuroneko@gmail.com>
2024-11-18 07:04:58 +0900

    feat: Storybookを導入

---
+++ .storybook/main.ts
@@ -0,0 +1,23 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+
+const config: StorybookConfig = {
+  features: {
+    experimentalRSC: true,
+  },
+  stories: [
+    "../app/**/*.stories.@(js|jsx|ts|tsx)",
+    "../components/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  staticDirs: ["../public"],
+};
+export default config;
