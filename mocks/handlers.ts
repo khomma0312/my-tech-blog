64a8c87d38ec420fcbf850ca164d22f1e6483df0
khomma0312 <kijikuroneko@gmail.com>
2024-11-21 07:13:53 +0900

    chore: モック周りの整理

--- mocks/handlers.ts
+++
@@ -1,8 +0,0 @@
-import { http, HttpResponse, type RequestHandler } from "msw";
-import { zennArticlesMock } from "./data/zenn/articles";
-
-const zennArticlesHandler = http.get("https://zenn.dev/api/articles", () => {
-  return HttpResponse.json(zennArticlesMock());
-});
-
-export const handlers: RequestHandler[] = [zennArticlesHandler];
