80d42d6b5d0081479a60f4c2b5ee12165ba2ea08
khomma0312 <kijikuroneko@gmail.com>
2024-11-21 07:24:27 +0900

    chore: 不要なコメントを削除

--- __mocks__/fs/index.ts
+++ __mocks__/fs/index.ts
@@ -1,6 +1,3 @@
 // https://vitest.dev/guide/mocking#file-system
 import { fs } from "memfs";
 export default fs;
-
-// const { fs } = require("memfs");
-// module.exports = fs;
--- __mocks__/fs/promises.ts
+++ __mocks__/fs/promises.ts
@@ -1,5 +1,2 @@
 import { fs } from "memfs";
 export default fs.promises;
-
-// const { fs } = require("memfs");
-// module.exports = fs.promises;
