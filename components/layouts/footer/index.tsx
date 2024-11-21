7ce11c405a7bd42c1cec2c89cbe6f6cdcb581fa1
khomma0312 <kijikuroneko@gmail.com>
2024-11-22 06:43:09 +0900

    chore: サイト名を仮置きのものから修正、記事画面の線を削除

--- components/layouts/footer/index.tsx
+++ components/layouts/footer/index.tsx
@@ -1,13 +1,10 @@
-import Link from "next/link";
-
 const Footer = () => {
   return (
     <footer className="p-4 border-t border-slate-500">
       <nav className="flex flex-col items-center text-muted">
-        <Link href="/policy" className="hover:text-primary">
-          Privacy Policy
-        </Link>
-        <small>&copy; {new Date().getFullYear()} by homma koharu</small>
+        <p className="text-sm">
+          &copy; {new Date().getFullYear()} {process.env.SITE_NAME}
+        </p>
       </nav>
     </footer>
   );
